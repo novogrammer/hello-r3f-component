46c1587536458f21defd81b1cf704cf3e9f5147d
Yusuke Kawamoto <novogrammer@gmail.com>
2024-01-14 20:06:50 +0900

    r3f

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Canvas } from '@react-three/fiber';
 import './App.css'
+import { Float, PerspectiveCamera, StatsGl } from '@react-three/drei';
+import { Suspense } from 'react';
+
+function GlobalScene(){
+  return  <>
+    <color attach="background" args={["green"]}/>
+    <PerspectiveCamera makeDefault position={[0,0,5]} fov={30} />
+    <ambientLight intensity={0.6} />
+    <directionalLight intensity={1.0} position={[0, 3, 5]}/>
+    <Suspense fallback={null}>
+      <Float rotationIntensity={10}>
+        <mesh>
+          <boxGeometry/>
+          <meshStandardMaterial color="orange"/>
+        </mesh>
+      </Float>
+    </Suspense>
+  </>;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{
+        position:"fixed",
+        left:0,
+        top:0,
+        width:"100%",
+        height:"100%",
+      }}>
+        <Canvas>
+          <StatsGl/>
+          <GlobalScene/>
+        </Canvas>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
