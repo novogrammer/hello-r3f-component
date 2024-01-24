a805097440249f4d19860fdb89ccd7e451cdef14
Yusuke Kawamoto <novogrammer@gmail.com>
2024-01-24 22:09:45 +0900

    他のオブジェクトを設置

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,25 @@ function GlobalScene(){
     <color attach="background" args={["green"]}/>
     <PerspectiveCamera makeDefault position={[0,0,5]} fov={30} />
     <ambientLight intensity={0.6} />
-    <directionalLight intensity={1.0} position={[0, 3, 5]}/>
+    <directionalLight intensity={1.0} position={[1, 1, 1]}/>
     <Suspense fallback={null}>
-      <Float rotationIntensity={10}>
-        <mesh>
+      <Float rotationIntensity={1}>
+      <mesh>
           <boxGeometry/>
           <BlobMaterial color={new THREE.Color(0xff00ff)}/>
           {/* <meshStandardMaterial color="orange"/> */}
+        </mesh>
+        <mesh position={[0,0,-1]}>
+          <boxGeometry args={[1,1,1]}/>
+          <meshStandardMaterial color="white"/>
+        </mesh>
+        <mesh position={[0,-1,0]} >
+          <boxGeometry args={[1,1,1]}/>
+          <meshStandardMaterial color="white"/>
+        </mesh>
+        <mesh position={[-1,0,0]} >
+          <boxGeometry args={[1,1,1]}/>
+          <meshStandardMaterial color="white"/>
         </mesh>
       </Float>
     </Suspense>
